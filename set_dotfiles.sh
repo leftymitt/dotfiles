fbbb681c7d14d2c675048419c572d0c966ed6c84
yellow <treefrog@riseup.net>
2016-04-04 11:57:25 +0000

    rename files to configs

--- set_dotfiles.sh
+++ set_dotfiles.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
-# copy dotfiles in files/ to the base of the user home directory
+# copy dotfiles in files/ to the base of the user home directory.
 
-FILE_DIR="files"
+FILE_DIR="configs"
 
 if ! [ -d "$FILE_DIR" ]; then
 	echo "run this script within the base of the git repository."
 	exit
 fi
 
-cd $FILE_DIR
+cd $FIlE_DIR
 
 for FILE in *; do
 	if [ -f $FILE ]; then
 		echo "copying $FILE..."
 		if [ -f  "$HOME/.$FILE" ]; then
-			echo "$FILE already exists. backing up..."
-			mv "$HOME/.$FILE" "$HOME/.$FILE""_$(date +%F)"
+			echo "$FILE already exists. opening vimdiff."
+			vimdiff "$FILE" "$HOME/.$FILE"
 		fi
 		cp "$FILE" "$HOME/.$FILE"
 	fi
